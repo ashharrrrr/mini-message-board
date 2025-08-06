0ec3740f38aed10bd2847d0a5b7b8381cfd7b2be
Ashhar Siddiqui <ashhar2002.6@gmail.com>
2025-08-06 17:26:39 +0530

    add message details page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -8,11 +8,23 @@ export async function getForm(req, res) {
     res.render("form");
 }
 
+let nextID = 3;
+
 export async function addMessage(req, res){
     const userName = req.body.username;
     const message = req.body.message;
 
-    messages.push({text: message, user: userName, added: new Date()})
+    messages.push({ id: nextID++, text: message, user: userName, added: new Date()})
 
     res.redirect("/");
+}
+
+export async function getMessageDetails(req, res){
+    const { messageId } = req.params
+    const messageDetails = messages.find((message) => message.id === Number(messageId)) 
+    if(!messageDetails){
+        console.log("Error: Path not found");
+    }
+
+    res.render("messageDetails", { messageDetails: messageDetails})
 }
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { homeController, getForm, addMessage } from "../controllers/homeController.js";
+import { homeController, getForm, addMessage, getMessageDetails } from "../controllers/homeController.js";
 
 const indexRouter = Router();
 
 indexRouter.get("/", homeController);
 indexRouter.get("/new", getForm);
+indexRouter.get("/messageDetails/:messageId", getMessageDetails);
 indexRouter.post("/new", addMessage);
 
 export default indexRouter;
